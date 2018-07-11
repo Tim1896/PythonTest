a1cc151b2653017272a15aa288120e0f4763d58d
Tim <schmidt.tim2001@gmail.com>
2018-07-11 13:28:09 +0200

    1.1.1
    Compare files added

--- Test.py
+++ Test.py
@@ -11,10 +11,17 @@ soup = BeautifulSoup(page, "html.parser")
 def readfnc(market):
     if market in "Bitstamp":
         for child in soup.find(id="market_bitstampbtcusd"):
+            bitstampcmp(child)
+            writebitstamp(child)
             return print("Bitstamp USD/BTC:", child)
     if market in "Bitfinex":
         for child in soup.find(id="market_bitfinexbtcusd"):
+            bitfinexcmp(child)
+            writebitfinex(child)
             return print("Bitfinex USD/BTC:", child)
+    else:
+        print("This market does not exist.")
+        start()
 
 
 def checklength(checkstring):
@@ -24,6 +31,47 @@ def checklength(checkstring):
         print("Please type at least 4 Letters.")
         start()
 
+
+def bitstampcmp(tocompare):
+    with open("bitstampcmp.txt", "r") as f:
+        lines = f.read().splitlines()
+        if len(lines) == 0:
+            print("nothing to compare")
+        else:
+            lastline = lines[-1]
+            if lastline > tocompare:
+                print("↓")
+            elif lastline < tocompare:
+                print("↑")
+            else:
+                print("→")
+
+
+def bitfinexcmp(tocompare):
+    with open("bitfinexcmp.txt", "r") as f:
+        lines = f.read().splitlines()
+        if len(lines) == 0:
+            print("nothing to compare")
+        else:
+            lastline = lines[-1]
+            if lastline > tocompare:
+                print("↓")
+            elif lastline < tocompare:
+                print("↑")
+            else:
+                print("→")
+
+
+def writebitstamp(towrite):
+    wrtfile = open("bitstampcmp.txt", "a")
+    wrtfile.write("%s\n" % towrite)
+    wrtfile.close()
+
+
+def writebitfinex(towrite):
+    wrtfile = open("bitfinexcmp.txt", "a")
+    wrtfile.write("%s\n" % towrite)
+    wrtfile.close()
 
 
 def start():
