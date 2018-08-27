7a6e13bb687ff59e964418ffb66112d50ab7c65a
Tim <schmidt.tim2001@gmail.com>
2018-08-27 10:46:36 +0200

    added delete function
    The Function deletes the Values to Compare if there are 10 or more.

--- Test.py
+++ Test.py
@@ -8,15 +8,37 @@ page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, "html.parser")
 
 
+def bitstampdel():
+    with open("bitstampcmp.txt", "r") as f:
+        lines = f.read().splitlines()
+        if len(lines) > 9:
+            f.close()
+            w = open("bitstampcmp.txt", "w")
+            for line in lines:
+                w.write("")
+
+
+def bitfinexdel():
+    with open("bitfinexcmp.txt", "r") as f:
+        lines = f.read().splitlines()
+        if len(lines) > 9:
+            f.close()
+            w = open("bitfinexcmp.txt", "w")
+            for line in lines:
+                w.write("")
+
+
 def readfnc(market):
-    if market in "Bitstamp" or "bitstamp":
+    if market == "Bitstamp":
         for child in soup.find(id="market_bitstampbtcusd"):
             bitstampcmp(child)
+            bitstampdel()
             writebitstamp(child)
             return print("Bitstamp USD/BTC:", child)
-    if market in "Bitfinex" or "bitfinex":
+    if market == "Bitfinex":
         for child in soup.find(id="market_bitfinexbtcusd"):
             bitfinexcmp(child)
+            bitfinexdel()
             writebitfinex(child)
             return print("Bitfinex USD/BTC:", child)
     else:
@@ -63,7 +85,7 @@ def bitfinexcmp(tocompare):
 
 
 def writebitstamp(towrite):
-    with open("bitfinexcmp.txt", "a") as f:
+    with open("bitstampcmp.txt", "a") as f:
         f.write("%s\n" % towrite)
     f.close()
 
